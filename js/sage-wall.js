f82b7ae646b826557ce92f2f91dde753b4f01d8b
Sage Wall <sagewall@users.noreply.github.com>
2015-09-25 06:01:17 -0600

    added resume and basic angular esri map

---
+++ js/sage-wall.js
@@ -0,0 +1,64 @@
+/**
+ * Created by Sage on 9/9/2015.
+ */
+(function(){
+    var app = angular.module('sage-wall', ['esri.map', 'ui.bootstrap']);
+
+    app.controller('MapController', function ($scope) {
+        $scope.map = {
+            center: {
+                lng: -95.000,
+                lat: 38.000
+            },
+            zoom: 4,
+            mapOptions: {
+                minZoom: 0,
+                maxZoom: 20,
+                resizeDelay: 500,
+                navigationMode: 'classic',
+                sliderOrientation: 'vertical',
+                sliderPosition: 'top-right',
+                displayGraphicsOnPan: true,
+                fadeOnZoom: false,
+                logo: true,
+                wrapAround180: true
+            }
+        };
+    });
+
+    app.controller('ProjectsController', ['$http', function($http){
+        var projectsCtrl = this;
+        projectsCtrl.projects = [];
+        $http.get('data/projects.json').success(function(data){
+            projectsCtrl.projects = data;
+        });
+    }]);
+
+    app.controller('ReportsController', ['$http', function($http){
+        var projectsCtrl = this;
+        projectsCtrl.reports = [];
+        $http.get('data/reports.json').success(function(data){
+            projectsCtrl.reports = data;
+        });
+    }]);
+
+    app.controller('baseballAppController', ['$http', function(){
+
+    }]);
+
+    app.directive("projectDescription", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "project-description.html"
+        };
+    });
+
+    app.directive("reportsDescription", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "reports-description.html"
+        };
+    });
+
+
+})();
